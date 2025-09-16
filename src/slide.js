1dbb74a5dac9ca97d4e309c9c54ffe72865e2c1d
soeunSim <629943@naver.com>
2025-09-16 15:43:55 +0900

    feat[slide] 네비게이터 이벤트 및 auto기능 추가

--- src/slide.js
+++ src/slide.js
@@ -34,10 +34,12 @@
 
 const slideList = document.querySelectorAll(".js-slide > .js-item");
 const slideArea = document.querySelector(".js-slide");
+const slideNavigator = document.querySelector('.box-navigator');
 
 let currentIndex = 0;
 let prevIndex = convertIndexLimit(currentIndex - 1);
 let nextIndex = convertIndexLimit(currentIndex + 1);
+let slideInterval = null;
 
 // 인덱스의 제한을 전환하는 함수
 function convertIndexLimit(index) {
@@ -84,7 +86,6 @@ function clickSlideEvent(direction) {
 
     // 현재 이벤트 방향 기준 해당 방향 및 현재 방향의 인덱스 재계산
     if (direction === "next") {
-      // 현재가 
       currentIndex = convertIndexLimit(targetIndex);
       prevIndex = convertIndexLimit(targetIndex - 1);  
       nextIndex = -1;      
@@ -110,14 +111,96 @@ function clickSlideEvent(direction) {
       prevIndex = convertIndexLimit(currentIndex - 1);
 
       setSlideItemClass(); 
-      
+
+      slideAuto();
     }, 500);
-    console.log(currentIndex);
   }
 }
 
+
+// 슬라이드 갯수를 가진 만큼 버튼을 보여주는 함수
+function createNavigationBtn () {
+  // 슬라이드 갯수를 알고 해당 갯수만큼 btn ele를 만들어준다.
+  const slideLength = slideList.length;
+
+  if (slideNavigator) {
+  // count만큼 반복하여 요소 생성 및 추가
+    for (let i = 0; i < slideLength; i++) {
+      // div 요소를 생성합니다.
+      const newElement = document.createElement('button');
+      newElement.classList.add('.nav-item')
+      // 생성된 요소에 텍스트를 추가할 수 있습니다.
+      newElement.textContent = `버튼 ${i + 1}`;
+      newElement.dataset['index'] = i;
+      newElement.addEventListener("click", clickSlideBtnEvent);
+      // 생성된 요소를 부모 요소에 추가합니다.
+      slideNavigator.appendChild(newElement);
+    }
+  }
+}
+
+// 슬라이드 안 btn을 click했을 떼
+// btn index와 일치하는 슬라이드로 이동하는 함수
+function clickSlideBtnEvent() {
+  const indexValue = parseInt(this.dataset['index']); // 당연하게 생각말고 찍어보자
+
+  if (slideArea.classList.contains('--transition') === false && currentIndex !== indexValue) {
+    // frame 01
+    // 내가 선택한 값이 현재값이면 변동이 없음, 현재 값과 다른 index여야 함. 
+    nextIndex = indexValue; 
+    prevIndex = -1;
+
+    setSlideItemClass();
+
+    setTimeout(() => {
+        //frame 02
+        // 애니메이션 시작
+        slideArea.classList.add('--transition');
+        
+        prevIndex = currentIndex;
+        currentIndex = indexValue;
+        nextIndex = -1;
+
+        // 01에서 세팅해준 값 기준으로 class 붙이기
+        setSlideItemClass();
+
+        setTimeout(() => {
+            //frame 03
+            // 애니메이션 종료 
+            slideArea.classList.remove('--transition');
+
+            nextIndex = convertIndexLimit(currentIndex + 1);
+            prevIndex = convertIndexLimit(currentIndex - 1);
+            
+            setSlideItemClass();
+
+            slideAuto();
+        }, 500);
+      
+    });
+  }
+}
+
+// 슬라이드가 자동 재생하는 함수
+function slideAuto() {
+  // 3초마다 슬라이드가 실행이 되어야한다.
+  if (slideInterval !== null) {
+    clearInterval(slideInterval);
+  }
+
+  slideInterval = setInterval(() => {
+    clickSlideEvent("next");
+  }, 3000);
+
+ // return slideInterval;
+}
+
+
 window.onload = () => {
   setSlideItemClass();
+  createNavigationBtn();
+
+  slideAuto();
 
   const prevButton = document.querySelector(".prev");
   const nextButton = document.querySelector(".next");
